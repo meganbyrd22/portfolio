455b03fd163d77b1df7252be5be72e4f3602d299
meganbyrd22 <meganbyrd22@gmail.com>
2023-06-23 13:43:25 -0500

    Declare variables for burger menu     Add event listener to burger menu

--- byrd.js
+++ byrd.js
@@ -8,3 +8,12 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
