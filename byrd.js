8e2da0ee19079559fc8874dba1219067c26256fb
meganbyrd22 <meganbyrd22@gmail.com>
2023-06-29 10:02:24 -0500

    Add function to push contact form responses to google form

--- byrd.js
+++ byrd.js
@@ -28,3 +28,33 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+
+//Contact form 
+function postToGoogle() {
+  var field1 = $("#Name").val();
+  var field2 = $("#Email").val();
+  var field3 = $("#Phone").val();
+  var field4 = $("#Message").val();
+
+  $.ajax({
+    url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSe8uNC7qJywdRORzIvI1afcRlo6kWDGiMLb0wSOwBtOFSjZow/formResponse",
+      
+    //add your google form generated numbers below which are also the 'names' of your inputs     
+    data: {
+      "entry.517626622": field1,
+      "entry.935048337": field2,
+      "entry.1737955681": field3,
+      "entry.354351359": field4
+    },
+    type: "POST",
+    dataType: "xml",
+    success: function (d) {
+      $('#contact').trigger('reset');
+    },
+    error: function (x, y, z) {
+      $('#contact').trigger('reset');
+    }
+  });
+  return false;
+}
